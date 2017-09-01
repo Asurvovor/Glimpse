28da110f805b77fa1f9ca4b2582aabf047d7981b
zjl <913065404@qq.com>
2017-09-01 16:34:54 +0800

    baseTools

---
+++ src/baseTools.js
@@ -0,0 +1,9 @@
+/**
+  * 哥哥是个上来就传参就跑的函数
+  */
+(function baseTools(win,undefined) {
+    // body...
+    win.basetool = "起飞";
+alert(basetool)
+
+})(window);
---
+++ src/readme.md
@@ -0,0 +1,7 @@
+
+
+# core source codes 
+
+## all codes is here 
+
+## it is the original
